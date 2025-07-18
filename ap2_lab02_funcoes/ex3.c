18e26b96315c304a2d99536eccbf482103c03716
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-18 13:56:19 -0300

    feito!

--- ap2_lab02_funcoes/ex3.c
+++ ap2_lab02_funcoes/ex3.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int verifica(int n) {
+    if (n > 0)
+        return 1;
+    else if (n < 0)
+        return -1;
+    else
+        return 0;
+}
+
+int main() {
+    int num, resultado;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    resultado = verifica(num);
+
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
