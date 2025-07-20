974e4a7f3ee227f5a927ef185062155b19ec3de0
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-19 23:23:41 -0300

    Código atualizado para caso o usuário entre com algo diferente de um número.

--- ap2_lista01_revisao/ex1.c
+++ ap2_lista01_revisao/ex1.c
@@ -5,7 +5,10 @@ int main(){
     float altura;
 
     printf("Digite a altura: ");
-    scanf("%f", &altura);
+    if (scanf("%f", &altura) != 1){
+        printf("Invalido, entre com um numero.\n");
+        return 1;
+    };
 
     if (altura <= 1.50){
         printf("Muito baixo!\n");
