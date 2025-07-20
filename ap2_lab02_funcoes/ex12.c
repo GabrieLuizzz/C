7e7c371b26ddff1cd9957815c0bcb8ee1c9659df
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-20 14:44:43 -0300

    resolvido!

---
+++ ap2_lab02_funcoes/ex12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int somadealgarismos(int n){
+    if(n>0){
+        return n%10 + somadealgarismos(n/10);
+    } else if(n==0) {
+        return 0;
+    } else {
+        printf("Numero invalido.\n");
+        return 0;
+    }
+}
+int main(){
+    int n, soma;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    soma = somadealgarismos(n);
+
+    printf("A soma dos algarismos eh: %d\n", soma);
+    return 0;
+}
