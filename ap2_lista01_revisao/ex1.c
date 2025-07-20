2cdb2c73a55892ee0079270279fdf0ab2080d16c
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-19 23:18:19 -0300

    código otimizado!

--- ap2_lista01_revisao/ex1.c
+++ ap2_lista01_revisao/ex1.c
@@ -9,11 +9,11 @@ int main(){
 
     if (altura <= 1.50){
         printf("Muito baixo!\n");
-    } else if (1.50 < altura && altura <= 1.60){
+    } else if (altura <= 1.60){
         printf("Baixo!\n");
-    } else if (1.60 < altura && altura <= 1.80){
+    } else if (altura <= 1.80){
         printf("Mediano!\n");
-    } else if (1.80 < altura){
+    } else if (altura > 1.80){
         printf("Alto!\n");
     } else {
         printf("Altura invalida!\n");
