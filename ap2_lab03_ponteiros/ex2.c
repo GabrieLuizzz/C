996c0faa182fee8bd1ad4d62c0929bfaad9d149a
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-25 16:23:30 -0300

    Resolvido!

---
+++ ap2_lab03_ponteiros/ex2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int var1 = 10, var2 = 15;
+
+    printf("Endereco de var1: %p\n", &var1);
+    printf("Endereco de var2: %p\n", &var2);
+    
+    if(&var1 > &var2){
+        printf("Maior endereco: %p\n", &var1);
+    } else {
+        printf("Maior endereco: %p\n", &var2);
+    }
+    return 0;
+}
