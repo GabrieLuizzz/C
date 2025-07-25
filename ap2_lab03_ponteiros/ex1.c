e75026863e99160862da8d5bc1e24f56d5492c29
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-25 15:36:35 -0300

    Primeiro exercício feito!

---
+++ ap2_lab03_ponteiros/ex1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x = 10;
+    float y = 5.0;
+    char z = 'A';
+    int  *ponteirox;
+    float *ponteiroy;
+    char *ponteiroz;
+
+    ponteirox = &x;
+    ponteiroy = &y;
+    ponteiroz = &z;
+
+    printf("_ANTES DA MODIFICACAO_");
+    printf("Valor de x: %d\n", *ponteirox);
+    printf("Valor de y: %.2f\n", *ponteiroy);
+    printf("Valor de z: %c\n", *ponteiroz);
+
+    *ponteirox = 20;
+    *ponteiroy = 10.5;
+    *ponteiroz = 'B';
+
+    printf("_DEPOIS DA MODIFICACAO_");
+    printf("Valor de x: %d\n", *ponteirox);
+    printf("Valor de y: %.2f\n", *ponteiroy);
+    printf("Valor de z: %c\n", *ponteiroz);
+}
