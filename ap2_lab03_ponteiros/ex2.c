42a3074e0599d69acee8f5f138c805122b2d6dca
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-28 23:23:50 -0300

    feito!

--- ap2_lab03_ponteiros/ex2.c
+++ ap2_lab03_ponteiros/ex2.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    int var1 = 10, var2 = 15;
+    int var1 = 10, var2 = 15, *p1, *p2;
 
     printf("Endereco de var1: %p\n", &var1);
     printf("Endereco de var2: %p\n", &var2);
-    
+
     if(&var1 > &var2){
         printf("Maior endereco: %p\n", &var1);
     } else {
